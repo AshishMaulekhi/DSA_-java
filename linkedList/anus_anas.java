7cd26fb157f902bd008ff9abfb57596261fdb687
ashish <ashishmaulekhi12@gmail.com>
2024-04-22 12:32:43 +0530

    znas gandu h

---
+++ linkedList/anus_anas.java
@@ -0,0 +1,7 @@
+package linkedList;
+
+public class anus_anas {
+    public static void main(String[] args) {
+        System.out.println("anas ka chota h ");
+    }
+}
--- linkedList/linked_list.java
+++
@@ -1,7 +0,0 @@
-package linkedList;
-
-public class linked_list {
-    public static void main(String[] args) {
-        System.out.println("dil mat todo yar anas ka");
-    }
-}
