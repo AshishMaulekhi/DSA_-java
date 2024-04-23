d4fd9af871f39fb1cadec679cb30cc1b3e946490
ashish <ashishmaulekhi12@gmail.com>
2024-04-23 10:55:21 +0530

    second largest and smallest no

---
+++ sorting/insertion_sort.java
@@ -0,0 +1,16 @@
+package sorting;
+
+public class insertion_sort {
+    public static void main(String[] args) {
+        int [] array={5,4,3,6,7,1};
+        int n=array.length;
+        insertionSort(array,n);
+    }
+    public static void insertionSort(int[] arr,int n){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<n;j++){
+
+            }
+        }
+    }
+}
