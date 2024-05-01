58c17894ee88930bb3a5d026bd0a4368b4211980
ashish <ashishmaulekhi12@gmail.com>
2024-05-01 10:44:30 +0530

    occur more than N/2 time

--- Arrays/occur_more_than_nby2.java
+++ Arrays/occur_more_than_nby2.java
@@ -8,7 +8,11 @@ public class occur_more_than_nby2 {
         //find the number in the array which is occurance more than n/2;
         //[1,2,3,2,2,1,2,5,2,7,5,2,2]----->here (2) come more than n/2;
         int[]array={1,2,3,2,2,1,2,5,2,2,7,5,2,2};
-        System.out.println(findNumber(array));
+       // System.out.println(findNumber(array));
+        //another way
+        //optimal solution--->moore's voting algo
+        System.out.println(findnum_(array));
+
     }
     public static int findNumber(int[]arr){
 
@@ -24,5 +28,36 @@ public class occur_more_than_nby2 {
             }
         }
         return -1;
+    }
+
+    public static int findnum_(int[]arr){
+        int n=arr.length;
+       // [1,2,3,2,2,1,2,5,2,2,7,5,2,2]
+        int element=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(count==0) {//for starting element of each sub array
+                count = 1;//for first element of each sub array
+                element = arr[i];//maan lo starting element hi result h
+            }
+            else if(arr[i]==element){//ager agla element hi same h to count ki value increase ker denge
+                count++;
+            }
+            else{//ager value same nhi h to ek subtract ker denge
+                count--;
+            }
+        }
+        int count_1=0;//ise count kerenge ki result wala element array m kitne bar aa rha h
+        for(int j=0;j<n;j++) {
+            if(element==arr[j]){//yha pe element ki value wo hogi jiski previous loop me count ki value zero nhi hui h
+                count_1++;
+            }
+        }
+        if(count_1>n/2){
+            return element;
+        }
+
+
+        return -1;//in  case we can't find any result
     }
 }
