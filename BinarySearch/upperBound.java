6100445575875eaa1d87559cd1c730551a2c92e7
ashish <ashishmaulekhi12@gmail.com>
2024-05-12 20:21:59 +0530

    upper bound of sorted array

---
+++ BinarySearch/upperBound.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+public class upperBound {
+    public static void main(String[] args) {
+        int[]array={3,5,8,9,9,15,19};
+        int target=9;
+        System.out.println(upperBoundOfArray(array,target));
+    }
+
+    public static int upperBoundOfArray(int[]arr,int target){
+        int n=arr.length;
+        //upper bound----> will be the index of that element (  arr[i]>target  )
+        //3,5,8,9,9,15,19
+        //if my target element is 19 than i return n because there is no other element
+        //which is greater than 19 so we return length of array which is n;
+        for(int i=0;i<n;i++){
+            if(arr[i]>target){
+                return i;
+            }
+        }
+        return n;
+    }
+}
