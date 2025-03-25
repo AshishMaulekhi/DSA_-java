6aee2c21f862f7e99c2ec1cf340afb3e97eef043
Ashish Maulekhi <ashishmaulekhi12@gmail.com>
2025-03-25 22:05:26 +0530

    Added Strings folder

---
+++ src/Strings/ReverseString.java
@@ -0,0 +1,12 @@
+package src.Strings;
+
+public class ReverseString {
+    public static String getUpperCase(String str){
+        return str.toUpperCase();
+
+    }
+    public static void main(String[] args) {
+        String given="rani ji ";
+        System.out.println(getUpperCase(given));
+    }
+}
