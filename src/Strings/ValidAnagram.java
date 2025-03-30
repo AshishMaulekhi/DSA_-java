3f76fa17e4e10d64d5286c98b29d27b2a0ba4e7f
Ashish Maulekhi <ashishmaulekhi12@gmail.com>
2025-03-30 10:20:52 +0530

    Valid Anagram

---
+++ src/Strings/ValidAnagram.java
@@ -0,0 +1,19 @@
+package src.Strings;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] sArr = s.toCharArray(), tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        return Arrays.equals(sArr, tArr);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram")); // true
+        System.out.println(isAnagram("rat", "car")); // false
+    }
+}
+
