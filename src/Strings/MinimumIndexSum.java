55bc8af2c5f61292d74c0522f54c105876f3f595
Ashish Maulekhi <ashishmaulekhi12@gmail.com>
2025-03-31 23:12:27 +0530

    Minimum Index Sum

---
+++ src/Strings/MinimumIndexSum.java
@@ -0,0 +1,36 @@
+package src.Strings;
+
+import java.util.*;
+
+public class MinimumIndexSum {
+    public static String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<>();
+        List<String> result = new ArrayList<>();
+        int minSum = Integer.MAX_VALUE;
+
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
+
+        for (int j = 0; j < list2.length; j++) {
+            if (map.containsKey(list2[j])) {
+                int sum = j + map.get(list2[j]);
+                if (sum < minSum) {
+                    minSum = sum;
+                    result.clear();
+                    result.add(list2[j]);
+                } else if (sum == minSum) {
+                    result.add(list2[j]);
+                }
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) {
+        String[] list1 = {"Shogun", "Tapioca Express", "Burger King", "KFC"};
+        String[] list2 = {"Piatti", "Shogun", "Burger King"};
+        System.out.println(Arrays.toString(findRestaurant(list1, list2)));
+        // Output: ["Shogun"]
+    }
+}
