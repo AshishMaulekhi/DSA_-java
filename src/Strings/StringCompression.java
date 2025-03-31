0f7f9a19ddb821a9e5122a767e86a8027a588cf3
Ashish Maulekhi <ashishmaulekhi12@gmail.com>
2025-03-31 23:14:21 +0530

    String compression

---
+++ src/Strings/StringCompression.java
@@ -0,0 +1,28 @@
+package src.Strings;
+
+public class StringCompression {
+    public static int compress(char[] chars) {
+        int index = 0, i = 0;
+        while (i < chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+            while (i < chars.length && chars[i] == currentChar) {
+                count++;
+                i++;
+            }
+            chars[index++] = currentChar;
+            if (count > 1) {
+                for (char c : Integer.toString(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        char[] chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+        System.out.println(compress(chars)); // Output: 6
+    }
+}
+
