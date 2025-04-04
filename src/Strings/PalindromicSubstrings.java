2a863e51fe9c0116f18ed54524fd3b39c056821b
AshishMaulekhi <ashishmaulekhi12@gmail.com>
2025-04-04 19:21:04 +0530

    /PalindromicSubstrings

---
+++ src/Strings/PalindromicSubstrings.java
@@ -0,0 +1,26 @@
+package src.Strings;
+
+public class PalindromicSubstrings {
+    public static int countSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count += expandAroundCenter(s, i, i);
+            count += expandAroundCenter(s, i, i + 1);
+        }
+        return count;
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countSubstrings("aaa")); // Output: 6
+    }
+}
