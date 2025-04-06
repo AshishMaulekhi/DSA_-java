b1ef6a7585453b0a554f170b8575e1a380e5b9fd
AshishMaulekhi <ashishmaulekhi12@gmail.com>
2025-04-06 18:22:17 +0530

    Linked List Cycle

---
+++ src/Linkedlist/LinkedListCycle.java
@@ -0,0 +1,24 @@
+package src.Linkedlist;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head, fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+        LinkedListCycle obj = new LinkedListCycle();
+        System.out.println(obj.hasCycle(head));
+    }
+}
