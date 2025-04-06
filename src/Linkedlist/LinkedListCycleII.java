91c2c51cfcf713adf6b37ebeadda41cd8af9c420
AshishMaulekhi <ashishmaulekhi12@gmail.com>
2025-04-06 18:21:47 +0530

    Linked List Cycle 11

---
+++ src/Linkedlist/LinkedListCycleII.java
@@ -0,0 +1,30 @@
+package src.Linkedlist;
+
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+        LinkedListCycleII obj = new LinkedListCycleII();
+        System.out.println(obj.detectCycle(head).val);
+    }
+}
